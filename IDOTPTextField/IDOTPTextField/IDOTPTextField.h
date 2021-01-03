60eb5f2d7dfaa91547a171fb237cab638f17b562
IrelDev <ireldeveloper@gmail.com>
2021-01-03 18:03:03 +0500

    Add project template

---
+++ IDOTPTextField/IDOTPTextField/IDOTPTextField.h
@@ -0,0 +1,18 @@
+//
+//  IDOTPTextField.h
+//  IDOTPTextField
+//
+//  Created by Kirill Pustovalov on 03.01.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for IDOTPTextField.
+FOUNDATION_EXPORT double IDOTPTextFieldVersionNumber;
+
+//! Project version string for IDOTPTextField.
+FOUNDATION_EXPORT const unsigned char IDOTPTextFieldVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IDOTPTextField/PublicHeader.h>
+
+
